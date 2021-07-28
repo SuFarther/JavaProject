5582fce5c8b0df8aa48b15490054caf3d251a5d8
xiaosu <2257346739@qq.com>
2021-07-28 16:35:48 +0800

    查询制定数组位置的索引

---
+++ src/com/company/array/SelectArrayPosition.java
@@ -0,0 +1,19 @@
+package com.company.array;
+
+/**
+ * @author 苏东坡
+ * @company 公司
+ * @create 2021-07-28-4:12 下午
+ * 数组优点: 在按照位置查询的时候,按照索引查询,效率非常高
+ */
+public class SelectArrayPosition {
+    public static void main(String[] args) {
+        /**
+         * 查询指定位置的元素
+         * 给定一个数组:
+         */
+        int[] arr = {12,48,288,17,20};
+        //查询索引为2位置上的元素
+        System.out.println(arr[2]);
+    }
+}
