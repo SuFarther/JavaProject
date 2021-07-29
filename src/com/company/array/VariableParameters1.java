8bd71a407a277372bfe887274717b47ac2bee139
xiaosu <2257346739@qq.com>
2021-07-29 08:52:40 +0800

    数组的可变参数作用提供一个方法,参数可变,解决部分方法的重载问题(不同类型除外)

--- src/com/company/array/VariableParameters1.java
+++ src/com/company/array/VariableParameters1.java
@@ -7,7 +7,6 @@ import sun.reflect.generics.tree.VoidDescriptor;
  * @company 公司
  * @create 2021-07-29-8:25 上午
  *
- *
  * 可变参数: 作用提供了一个方法, 参数的个数是可变的,解决了部分方法的重载问题
  * int...num
  * double...num
@@ -18,24 +17,24 @@ import sun.reflect.generics.tree.VoidDescriptor;
  */
 public class VariableParameters1 {
     public static void main(String[] args) {
-         method(10);
-         method();
-         method(10,20,30);
-         method(10,20,30,40);
-         method(new int[]{10,20,30});
-        method2(10,20,30,40);
+        method(10);
+        method();
+        method(10, 20, 30);
+        method(10, 20, 30, 40);
+        method(new int[]{10, 20, 30});
+        method2(10, 20, 30, 40);
     }
 
 
-    public static void method(int...num){
+    public static void method(int... num) {
         System.out.println("1-----");
-        for (int i:num){
-            System.out.print(i+"\t");
+        for (int i : num) {
+            System.out.print(i + "\t");
         }
         System.out.println();
     }
 
-    public static void method2(int num,int...num1){
+    public static void method2(int num, int... num1) {
         System.out.println("2---");
     }
 }
