1814e6a7119b73669488ff109fd7cb7a3c6dc77c
xiaosu <2257346739@qq.com>
2021-07-31 15:29:13 +0800

    面向对象-构造器重载(直接重写无参构造器,跟无参构造器起相同名字,只是参数列表不同,this指的是当前创建的对象)

---
+++ src/com/company/object/two/ConstructoLoad.java
@@ -0,0 +1,26 @@
+package com.company.object.two;
+
+/**
+ * @version 1.0
+ * @ClassName ConstructorReload
+ * @company 公司
+ * @Description 构造器重载
+ * @createTime 2021年07月31日 01:50:50
+ */
+public class ConstructoLoad {
+
+    String name;
+    int age;
+
+   public ConstructoLoad(){
+
+   }
+   public ConstructoLoad(String name, int age){
+       this.name = name;
+       this.age = age;
+   }
+
+    public void  eat(){
+        System.out.println("我喜欢吃饭");
+    }
+}
