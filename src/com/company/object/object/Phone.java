635e93026e4520ed0412dea668c27a1aae8b869a
xiaosu <2257346739@qq.com>
2021-08-02 21:48:39 +0800

    面向对象-Object类的equals()方法，子类重写父类的equals()方法

--- src/com/company/object/object/Phone.java
+++ src/com/company/object/object/Phone.java
@@ -1,6 +1,7 @@
 package com.company.object.object;
 
-import java.util.Date;
+
+import java.util.Objects;
 
 /**
  * @author 苏东坡
@@ -61,26 +62,40 @@ public class Phone {
      * 对equals进行重写
      * 子类对父类Object类不满意,方法进行重写
      *
-     * @param obj
+     * @param
      * @return
      */
+//    @Override
+//    public boolean equals(Object obj) {
+//        /**
+//         * instanceof前面的对象是否是后面一个类的实例
+//         *
+//         * a instanceof b
+//         * 判断a对象是否是b这个类的实例,如果是 返回true 如果不是返回false
+//         *
+//         * 属于Phone类的对象
+//         */
+//        if(obj instanceof Phone){
+//            Phone other = (Phone) obj;
+//            if (this.getBrand() == other.getBrand() && this.getPrice() == other.getPrice()
+//                    && this.getYear() == other.getYear()) {
+//                return  true;
+//            }
+//        }
+//        return false;
+//    }
+
+
     @Override
-    public boolean equals(Object obj) {
-        /**
-         * instanceof前面的对象是否是后面一个类的实例
-         *
-         * a instanceof b
-         * 判断a对象是否是b这个类的实例,如果是 返回true 如果不是返回false
-         *
-         * 属于Phone类的对象
-         */
-        if(obj instanceof Phone){
-            Phone other = (Phone) obj;
-            if (this.getBrand() == other.getBrand() && this.getPrice() == other.getPrice()
-                    && this.getYear() == other.getYear()) {
-                return  true;
-            }
-        }
-        return false;
+    public boolean equals(Object o) {
+        if (this == o){ return true;};
+        if (o == null || getClass() != o.getClass()){ return false;};
+        Phone phone = (Phone) o;
+        return Double.compare(phone.price, price) == 0 && year == phone.year && brand.equals(phone.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, price, year);
     }
 }
