c46742b63eb1bbb26e9a78ca78ff50441bdf15f9
xiaosu <2257346739@qq.com>
2021-08-05 20:23:09 +0800

    异常-多重catch

---
+++ src/com/company/exception/ExceptionManyCatchTest.java
@@ -0,0 +1,33 @@
+package com.company.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName ExceptionTryCatchTest
+ * @company 公司
+ * @Description 异常
+ * 多重catch
+ *
+ * @createTime 2021年08月05日 08:35:35
+ */
+public class ExceptionManyCatchTest {
+    public static void main(String[] args) {
+      try {
+          Scanner  s = new Scanner(System.in);
+          System.out.print("请录入一个数:");
+          int num1 = s.nextInt();
+          System.out.print("请录入二个数:");
+          int num2 = s.nextInt();
+          System.out.print("商:"+num1/num2);
+      }catch (InputMismatchException e){
+          System.out.println("对不起,你录入的数据不是int类型");
+      }catch (ArithmeticException e){
+          System.out.println("对不起,除数不为0");
+      } finally {
+          System.out.println("谢谢你使用计算器");
+      }
+    }
+}
