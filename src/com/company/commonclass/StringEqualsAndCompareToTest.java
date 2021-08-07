ded5d9b56047b40c9e3abb43afbe1ff707c0bda5
xiaosu <2257346739@qq.com>
2021-08-07 19:37:53 +0800

    String类中的常用方法

--- src/com/company/commonclass/StringEqualsAndCompareToTest.java
+++ src/com/company/commonclass/StringEqualsAndCompareToTest.java
@@ -1,5 +1,8 @@
 package com.company.commonclass;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 /**
  * @author 苏东坡
  * @version 1.0
@@ -16,16 +19,53 @@ package com.company.commonclass;
  *  String下面的compareTo方法：
  *  String类实现了Comparable,里面有一个抽象方法叫compareTo,所以String中一定要对这个方法进行重写
  *
+ *
+ *
+ *
  * @createTime 2021年08月07日 15:58:58
  */
 public class StringEqualsAndCompareToTest {
     public static void main(String[] args) {
         String str1 = new String("abc");
         String str2 = new String("abc");
-        System.out.println(str1.equals(str2));
+        System.out.println("String类中的equals: "+str1.equals(str2));
 
         String str3 = new String("abc");
         String str4 = new String("abc");
-        System.out.println(str3.compareTo(str4));
+        System.out.println("String类中的compareTo: "+str3.compareTo(str4));
+
+        ///截取字符串中的字符串,索引从第几位开始到第几位结束,如果传入的是第一位 [+,+)
+        String str5 = "helloworld";
+        System.out.println("String类中的substring: "+str5.substring(3,6));
+        //startsWith以什么开头,以endsWith什么结尾
+        if(str5.startsWith("h") && true&&str5.endsWith("d")){
+            System.out.println("startsWith和endsWith");
+        }
+
+        //判断某个某个字符串里面是否包含某个字符
+        System.out.println("String类的contain用法:"+str5.contains("h"));
+
+        //字符串的拼接
+        System.out.println("String类的concat用法:"+str5.concat("ddddd"));
+
+        //字符串的切割成一个数组
+        String str7 = "a-b-c-d-g";
+        System.out.println("String类的split切割用法:"+ Arrays.toString(str7.split("-")));
+
+
+        //字符串中的大小写转换
+        System.out.println("String类的toUpperCase用法将字母转成大写:"+str5.toUpperCase());
+        System.out.println("String类的toLowerCase用法将字母转成小写:"+str5.toUpperCase().toLowerCase());
+
+        //去除首尾空格空格
+        String str6 = "   sdsdad ";
+        System.out.println("String类的trim去除首尾空格空格:"+str6.trim());
+
+
+        //字符串的替换
+        System.out.println("String类的replace替换方法:"+str5.replace('h','u'));
+
+        //字符串中将String转换为类型
+        System.out.println(String.valueOf(false));
     }
 }
