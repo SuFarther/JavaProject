48410fb5e6c7292623348330615d562b03d3577c
xiaosu <2257346739@qq.com>
2021-08-22 13:23:32 +0800

    多线程第二种方法实现多线程抢票

---
+++ src/com/company/thread/ThreadImplementsRunnable2Test.java
@@ -0,0 +1,23 @@
+package com.company.thread;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName ThreadImplementsRunnable2
+ * @company 公司
+ * @Description 利用匿名内部类来创建对象
+ * @createTime 2021年08月22日 13:00:00
+ */
+class ThreadImplementsRunnable2Test{
+    public static void main(String[] args) {
+       Thread t = new Thread(new Runnable() {
+           @Override
+           public void run() {
+               for (int i = 1; i <= 10; i++) {
+                   System.out.println(Thread.currentThread().getName()+i);
+               }
+           }
+       });
+       t.start();
+    }
+}
