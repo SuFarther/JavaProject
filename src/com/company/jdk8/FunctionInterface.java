07cb277532d4fc5b6af523ccbf7f13633e5c16df
xiaosu <2257346739@qq.com>
2021-08-28 12:13:58 +0800

    函数式接口定义

---
+++ src/com/company/jdk8/FunctionInterface.java
@@ -0,0 +1,19 @@
+package com.company.jdk8;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName FunctionInterface
+ * @company 公司
+ * @Description 函数式接口
+ * 如果一个接口中,只声明了一个抽象方法，则接口称为函数式接口
+ *
+ * @createTime 2021年08月28日 12:10:10
+ */
+@FunctionalInterface
+public interface FunctionInterface {
+    /**
+     * 函数是接口
+     */
+    void  eat();
+}
